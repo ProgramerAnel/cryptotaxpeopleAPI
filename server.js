810822c285b40ce92db0e60c815893f20938ede8
Anel Hodžić <iml4203@Imel.Local>
2021-11-24 15:28:05 +0100

    Change heroku port

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database !'))
 
 app.use(express.json())
+app.set('port', (process.env.PORT || 5000));
+
 
 const logRouter = require('./routes/logs')
 app.use('/logs', logRouter)
 
-app.listen(3000, () => console.log('Server up and running !'))
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
